35a8f62903f7f7bd373e339f949455d2a7b2439e
JQ <jqocon@gmail.com>
2020-02-08 23:03:37 +0000

    chore: bump version

--- dist/models/data_wrappers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MinutesIntervals;
-(function (MinutesIntervals) {
-    MinutesIntervals[MinutesIntervals["30-min"] = 30] = "30-min";
-    MinutesIntervals[MinutesIntervals["1-h"] = 60] = "1-h";
-    MinutesIntervals[MinutesIntervals["4-h"] = 240] = "4-h";
-    MinutesIntervals[MinutesIntervals["24-h"] = 1440] = "24-h";
-})(MinutesIntervals = exports.MinutesIntervals || (exports.MinutesIntervals = {}));
