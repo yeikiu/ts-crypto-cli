08b5b6d5cf0598b6806c1a54ed96ea6e751a9e58
JQ <jqocon@gmail.com>
2020-07-08 17:49:23 +0100

    fix: getKrakenLastPrice

--- src/services/kraken/get_last_price.ts
+++ src/services/kraken/get_last_price.ts
@@ -1,15 +1,9 @@
 import { krakenPublicApiRequest } from "../../api_clients/kraken/public_api_request"
-import { stdOHLC } from "../../types/standard_ohlc"
 
-export const getKrakenLastPrice = async (pair: string): Promise<stdOHLC> => {
+export const getKrakenLastPrice = async (pair: string): Promise<number> => {
     const ticker = await krakenPublicApiRequest({ url: 'Ticker', data: { pair }})
     const [assetKey,] = Object.keys(ticker)
-    const { o: open, h: [high,], l: [low,], h: [close,] } = ticker[assetKey]
+    const { /* o: open, h: [high,], l: [low,],  */c: [close,] } = ticker[assetKey]
 
-    return {
-        open: Number(open),
-        high: Number(high),
-        low: Number(low),
-        close: Number(close),
-    }
+    return Number(close)
 }
